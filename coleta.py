2b24e3045bc9dbf5e89595266eb3cc37f2bfbdbb
jesiel364 <jesiel364@gmail.com>
2022-03-13 16:00:23 -0400

    add view port tag

--- coleta.py
+++ coleta.py
@@ -2,10 +2,29 @@ import csv
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get('https://www.americanas.com.br/categoria/automotivo/moto/pecas-para-motos')
+
+print('Loading...')
+try:
+	page = requests.get('https://www.crummy.com/software/BeautifulSoup/bs4/doc/')
+except:
+	print('Erro ao carregar a página.')
+	exit()
+
 
 soup = BeautifulSoup(page.text, 'html.parser')
-# Remover links inferiores
-lista_produtos = soup.find().all_all(p)
-for produto in lista_produtos:
-  print(produto)
+
+
+
+refs = soup.find(_class='reference internal')
+refs = refs.decompose()
+
+print(refs)
+exit()
+
+title = soup.title
+print(title) # Recuperando title
+	# Remover links inferiores	
+lista = soup.find('p')
+
+for item in lista:
+	print(item)
